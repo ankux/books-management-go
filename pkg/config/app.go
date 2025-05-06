c6ab273df4e0c79fd7959d8ee948da0cac7a0529
Ankux <ankuxkr@gmail.com>
2025-05-06 13:33:14 +0530

    routes and config setup

---
+++ pkg/config/app.go
@@ -0,0 +1,23 @@
+package config
+
+import (
+	"github.com/jinzhu/gorm"
+	"github.com/jinzhu/gorm/dialects/mysql"
+)
+
+var (
+	db *grom.DB
+)
+
+func Connect() {
+	d, err := gorm.Open("mysql", "ankush:ankush@123/books?charset=utf8&parseTime=True&loc=Local")
+	if err != nil {
+		panic(err)
+	}
+
+	db = d
+}
+
+func GetDB() *gorm.DB {
+	return db
+}
